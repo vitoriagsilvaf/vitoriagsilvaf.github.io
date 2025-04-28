7659962f08d4dc5ab7ef97526a7138c8c91bd89f
Vitória Gabriele <vitoriagaby49@gmail.com>
2025-04-28 09:46:12 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ import initMenuTransition from "./modules/menu-transition.js";
 import initScrollSuave from "./modules/scroll-suave.js";
 import initTabNav from "./modules/tabnav.js";
 import initTyping from "./modules/typing.js";
-import { showPdf } from './modules/showPdf.js';
 
 
 
